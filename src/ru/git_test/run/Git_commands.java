8eef6acdf7debc87450834b4e73dba2d1b0860d3
vadim <gabderahmanov99@gmail.com>
2017-05-26 19:47:19 +0400

    changed class git_commands

--- src/ru/git_test/run/Git_commands.java
+++ src/ru/git_test/run/Git_commands.java
@@ -1,11 +1,27 @@
 package ru.git_test.run;
 
+import java.io.File;
 import java.io.IOException;
-
-public class Git_commands {
-	public void gitAdd(String git_command) throws IOException {
-		ProcessBuilder pb = new ProcessBuilder(git_command);	
-		pb.command(git_command);
-		pb.start();
+public class Git_commands {	
+	public void gitInit(File path) throws IOException {
+		runCommand(path, "git init");
 	}
+	public void gitAdd(File path) throws IOException {
+		runCommand(path, "git add .");
+	}
+	public void gitCommit(File path) throws IOException {
+		runCommand(path, "git commit -m'init'");
+	}
+	public void gitPush(File path) throws IOException {
+		runCommand(path, "git push");
+	}
+	public void runCommand (File path, String commands){	
+		try {
+			ProcessBuilder pb = new ProcessBuilder().command(commands).directory(path);
+			Process p = pb.start();
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
+	
 }
--- src/ru/git_test/run/Run.java
+++ src/ru/git_test/run/Run.java
@@ -1,20 +1,13 @@
 package ru.git_test.run;
 
-import java.io.IOException;
 
-import javax.swing.JOptionPane;
+
 public class Run {
 
 	public static void main(String[] args) {
-		try {
-			Git_commands gc = new Git_commands();			
-			String git_run = "C:\\Program Files\\Git\\git-bash.exe";
-			Runtime.getRuntime().exec(git_run);
-			gc.gitAdd("git add .");
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, e.getStackTrace());
-			e.printStackTrace();
-		}
-	}
+
+		Git_commands gc = new Git_commands();
+		
+	}	
 
 }
