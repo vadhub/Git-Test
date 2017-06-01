d9c9c1fce00723cdd45334d1f34c7876cb47a2f8
vadim <gabderahmanov99@gmail.com>
2017-06-01 15:45:26 +0400

    add_button_goto_path

--- src/ru/git_test/run/Run.java
+++ src/ru/git_test/run/Run.java
@@ -1,10 +1,12 @@
 package ru.git_test.run;
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Run {
 
@@ -12,13 +14,18 @@ public class Run {
 		ActionButtons abs = new ActionButtons();	
 		
 		JFrame frame = new JFrame("Git-test");
+		
 		JPanel btn_panel = new JPanel();
+		JPanel txt_panel = new JPanel();
+		
 		JButton git = new JButton("git"); 
 		JButton init = new JButton("Init local repo");
 		JButton add = new JButton("Add code");
 		JButton commit = new JButton("Commit");
 		JButton push = new JButton("Push");
+		JButton goto_path = new JButton("goto path");
 		
+		JTextField path = new JTextField(10);
 		
 		btn_panel.setLayout(new FlowLayout());
 		btn_panel.add(git);
@@ -26,8 +33,15 @@ public class Run {
 		btn_panel.add(add);
 		btn_panel.add(commit);
 		btn_panel.add(push);
+		btn_panel.add(goto_path);
 		
-		frame.add(btn_panel);	
+		txt_panel.setLayout(new FlowLayout());
+		txt_panel.add(path);
+		
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(btn_panel, "North");
+		frame.add(txt_panel, "South");
 		frame.pack();
 		frame.setVisible(true);
 		frame.setLocation(455, 455);
